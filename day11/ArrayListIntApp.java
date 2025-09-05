aca6df51c563f95da6bb548da66e769e3f2ef325
∀suxh <154598044+vsuxh@users.noreply.github.com>
2025-09-05 15:59:53 +0900

    Add files via upload
    progerm files from day11added

---
+++ day11/ArrayListIntApp.java
@@ -0,0 +1,7 @@
+public class ArrayListIntApp {
+	public static void main(String[] args){
+		ArrayListInt list = new ArrayListInt();
+		{list.add(314); list.add(271); list.add(173);}
+		{list.print();}
+	}	
+}
---
+++ day11/Gakkyu.java
@@ -0,0 +1,26 @@
+public class Gakkyu {
+	private int saidai;
+	private int[] a;
+	private int jissai;
+	public Gakkyu() {
+		this.saidai = 5;
+		this.a = new int[this.saidai];
+		this.jissai = 0;
+	}
+	public void add(int x) {
+		if (this.jissai <= 5){
+			this.a[jissai] = x;
+			this.jissai +=1;
+		} else {System.out.println("格納上限ぞよ");}
+	}
+
+	public int getSize() {return this.jissai;}
+	public int getShell(int x) {return this.a[x];}
+
+	public void print(){
+		int i;
+		for(i=0;i<jissai;i++){
+			System.out.println(a[i]);
+		}
+	}
+}
