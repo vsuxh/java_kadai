e3cdee6adce3f67e2319dc8360df0daa90698bb7
∀suxh <154598044+vsuxh@users.noreply.github.com>
2026-01-21 14:24:35 +0900

    Add files via upload

---
+++ day22/model.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Arrays;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+public class model(){
+	public model{
+		private boolean[][] board = new boolen[4][4];
+		private Arrays.fill(board, false);
+		private Random rand = new Random();
+	}
+
+	private void pressCell[row][col]{
+        	if (board[row][col] = true)(board[row][col] = false;)
+        	else (board[row][col] = true;)
+        }
+
+	private void reset(){
+		model();
+		//pressCell[row][col]でrand使って複数回実行予定	
+	}
+
+	private void isCleared(){
+		private boolean isfalse = false;
+		if 
+	}
+}
---
+++ day22/view.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+public class view{
+
+} 
